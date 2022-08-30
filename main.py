fd885bc94cb89cddcd1033d3532d63276ff94b69
YupenBob <yupenbob@163.com>
2022-08-30 15:17:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ headers = {
      AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44"
 }
 
+#解析视频弹幕xml地址
 def DanmuURL_get(bv):
+    print("解析视频弹幕xml地址")
     video_url = "https://bilibili.com/video/BV" + bv
+    print("BV:"+video_url)
     response = requests.get(video_url, headers=headers)
     html = response.content.decode()
     cid = re.findall(r'("cid":)([0-9]+)', html)
     xml_url = "http://comment.bilibili.com/" + str(cid[0][1]) + ".xml"
     return xml_url
 
+#获取弹幕信息
 def Danmu_get(xml_url):
+    print("获取弹幕信息")
+    print("XML:"+xml_url)
     response = requests.get(xml_url, headers=headers)
     html = etree.HTML(response.content)
     danmu_list = html.xpath("//d/text()")
