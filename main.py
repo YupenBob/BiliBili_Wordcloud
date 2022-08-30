cc84319a63742a0964e0d19ff694637d841b1287
YupenBob <73699916+YupenBob@users.noreply.github.com>
2022-08-30 14:31:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from lxml import etree
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
@@ -10,17 +11,17 @@ def DanmuURL_get(bv):
     video_url = "https://bilibili.com/video/BV" + bv
     response = requests.get(video_url, headers=headers)
     html = response.content.decode()
-    cid = re.findall(r'(?<=("cid":))[0-9]+(?=(,))', html)
-    xml_url = "http://comment.bilibili.com/" + str(cid[0]) + ".xml"
+    cid = re.findall(r'("cid":)([0-9]+)', html)
+    xml_url = "http://comment.bilibili.com/" + str(cid[0][1]) + ".xml"
     return xml_url
 
-def Danmu_get(url):
-    response = requests.get(url, headers=headers)
-    html = response.content.decode()
-    danmu = re.findall('(?<=(<d p=".*?">)).*(?=(</d>))', html)
-    return danmu
+def Danmu_get(xml_url):
+    response = requests.get(xml_url, headers=headers)
+    html = etree.HTML(response.content)
+    danmu_list = html.xpath("//d/text()")
+    return danmu_list
 
 
-url = DanmuURL_get("1Qd4y1d7px")
-list = Danmu_get(url)
+xml_url = DanmuURL_get("1Qd4y1d7px")
+list = Danmu_get(xml_url)
 print(list)
